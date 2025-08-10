0254ddf75bb001c32133cad271defef6f410abae
Priya <priyadharshini.offl@gmail.com>
2025-08-10 15:46:48 +0530

    Singleton Pattern Implementation

--- Singleton.py
+++ Singleton.py
@@ -1,1 +1,13 @@
+class Singleton:
+    _instance = None  # keeps the only object we ever make.
 
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)  # First call → None → create it.
+        return cls._instance  # Later calls → return existing _instance.
+
+
+a = Singleton()
+b = Singleton()
+
+print(a is b)
